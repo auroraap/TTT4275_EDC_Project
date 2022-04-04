761f422aacc4c2624288da2e10e2a75587f86d77
Aurora <78878792+auroraap@users.noreply.github.com>
2022-04-04 12:45:57 +0200

    Update knn.m

--- knn.m
+++ knn.m
@@ -7,18 +7,22 @@ function nearestNeighbors = knn(k,observation,trainingSet,numProperties)
 %%%                observation                                     %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-distArray = NaN(size(trainingSet,1),2);     % initialize array of distances
+% initialize array of distances and differances
+distArray = NaN(size(trainingSet,1),2);
+diff      = NaN(size(trainingSet,1),numProperties);
 
 % Find difference between observation and each datapoint for each property
-for property=2:numProperties+1
-    diff = observation(property) - trainingSet(:,property);
+for dataPoint=1:size(trainingSet,1)
+    for property=1:numProperties
+        diff(dataPoint,property) = observation(property+1) - trainingSet(dataPoint,property+1);
+    end
 end
 
 % Find the distance between observation and each datapoint
-for datapoint=1:size(trainingSet,1)
-    id = trainingSet(datapoint,1);
-    dist = norm(diff(datapoint,:));
-    distArray(datapoint,:) = [id, dist];
+for dataPoint=1:size(trainingSet,1)
+    id = trainingSet(dataPoint,1);
+    dist = norm(diff(dataPoint,:));
+    distArray(dataPoint,:) = [id, dist];
 end
 
 % Sort and return the ID of the k nearest datapoints
