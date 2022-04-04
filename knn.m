98d6b2a5d5ad50e9f7c246d0aa430e713821fd84
Aurora <78878792+auroraap@users.noreply.github.com>
2022-04-04 11:35:18 +0200

    Add files via upload

---
+++ knn.m
@@ -0,0 +1,20 @@
+function nearestNeighbors = knn(k,observation,trainingSet)
+% observation and training set contains: [ID, properties, label]
+
+diffArray = NaN(size(trainingSet,1),2);
+
+for dataPoint=1:size(trainingSet,1)
+    diff = 0;
+    id = observation(1);
+    for property=2:size(trainingSet,2)-1
+        diff = diff + (observation(dataPoint,property) - trainingSet(dataPoint,property));
+    end
+    diffArray(dataPoint,:) = [id, diff];
+end
+
+diffArray = sort(diffArray,1);
+
+nearestNeighbors = diffArray(1:k,1);
+
+end
+
