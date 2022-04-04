3d984e486642bce6c853ccc37be5396a87d742d7
Aurora <78878792+auroraap@users.noreply.github.com>
2022-04-04 12:39:32 +0200

    endret fremgangsmåte men prinsippet er det samme
    Plz ignore me mens jeg eksperimenterer lol

--- knn.m
+++ knn.m
@@ -1,21 +1,28 @@
 function nearestNeighbors = knn(k,observation,trainingSet,numProperties)
-% observation and training set contains: [ID, properties, label]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% k: the number of neighbors used to classify the observation    %%%
+%%% observation: [ID, properties, label]                           %%%
+%%% trainingSet: [ID, properties, label] for each datapoint        %%%
+%%% numProperties: number of properties used to classify the       %%%
+%%%                observation                                     %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-diffArray = NaN(size(trainingSet,1),2);
-distanceArray = NaN(numProperties);
+distArray = NaN(size(trainingSet,1),2);     % initialize array of distances
 
-for dataPoint=1:size(trainingSet,1)
-    id = observation(1);
-    for i=1:numProperties
-        property = i+1;
-        distanceArray(i) = observation(dataPoint,property) - trainingSet(dataPoint,property);
-    end
-    diffArray(dataPoint,:) = [id, norm(distanceArray)];
+% Find difference between observation and each datapoint for each property
+for property=1:numProperties
+    diff = observation(:,property) - trainingSet(:,property);
 end
 
-diffArray = sort(diffArray,1);
-
-nearestNeighbors = diffArray(1:k,1);
-
+% Find the distance between observation and each datapoint
+for datapoint=1:size(trainingSet,1)
+    id = trainingSet(datapoint,1);
+    dist = norm(diff(datapoint,:));
+    distArray(datapoint,:) = [id, dist];
 end
 
+% Sort and return the ID of the k nearest datapoints
+distArray = sort(distArray,1);
+nearestNeighbors = distArray(1:k,1);
+
+end
