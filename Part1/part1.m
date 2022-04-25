955b9bda3667f9033c695b32d72704b6b97a6bfe
Aurora <78878792+auroraap@users.noreply.github.com>
2022-04-25 16:20:36 +0200

    Add files via upload

--- Part1/part1.m
+++ Part1/part1.m
@@ -6,13 +6,12 @@ n_tests = size(testSet,1);
 predLabel = NaN(n_tests,1);
 
 for testItem=1:n_tests
-    predLabel(testItem) = knn(5, testSet(testItem,:), trainingSet, 4);
+    predLabel(testItem) = knn(5, testSet(testItem,:), trainingSet);
 end
 
 %% Plot confusion chart
 close all;
 fig1 = figure;
 cm = confusionchart(testLabels_30s,predLabel);
-% cm.Title = '';
 cm.RowSummary = 'row-normalized';
 hgexport(fig1, 'part1_confusion.eps');
